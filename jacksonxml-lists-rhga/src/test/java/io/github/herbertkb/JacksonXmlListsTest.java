4ca933646f3846fa17d5e2a49a9424ec12cff839
Keith Herbert <kherbert@redhat.com>
2021-02-19 16:51:27 -0500

    unit test to demonstrate jacksonxml lists bug for FUSE and latest Camel versions

---
+++ jacksonxml-lists-rhga/src/test/java/io/github/herbertkb/JacksonXmlListsTest.java
@@ -0,0 +1,33 @@
+package io.github.herbertkb;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.test.junit4.CamelTestSupport;
+import org.junit.Test;
+
+public class JacksonXmlListsTest extends CamelTestSupport {
+    @Test
+    public void test() throws Exception {
+        MockEndpoint resultEndpoint = resolveMandatoryEndpoint("mock:result", MockEndpoint.class);
+        resultEndpoint.expectedMessageCount(1);
+
+        final String xml = "<root><list><item>ONE</item><item>TWO</item><item>THREE</item></list></root>";
+        template.sendBody("direct:jacksonxml", xml);
+
+        resultEndpoint.assertIsSatisfied();
+    }
+
+    @Override
+    protected RouteBuilder createRouteBuilder() {
+        return new RouteBuilder() {
+            public void configure() {
+                from("direct:jacksonxml")
+                    .unmarshal().jacksonxml()
+                    .log("Body: ${body}")
+                    .to("mock:result");
+            }
+        };
+    }
+}
